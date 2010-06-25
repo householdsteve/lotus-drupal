2483efbaeae6751e0e3b827765a3630a4d4f06ae
Steve McCrumb <steve@pofd.com>
2010-06-25 15:15:12 +0200

    last updates before work began hardcore again

--- sites/all/contemplates/node-page.tpl.php
+++ sites/all/contemplates/node-page.tpl.php
@@ -7,10 +7,12 @@ $options = array(
   );
 
 $nodeImgs = $node->content['field_page_images']['field']['items'][0]["#node"]->field_page_images;
-	
+
+  if($nodeImgs){	
 	foreach($nodeImgs as $item){
 		$imgs[] = $item['view'];
 	}
+  }
 
 $p = drupal_get_path('module', 'jcarousel')."/jcarousel/skins/lotus/skin.css";
 
