715cd484453e9d3388eaaed502114dfd60faec2c
Steve McCrumb <steve@pofd.com>
2010-09-05 17:17:27 +0200

    changes up to 9/4/2010

---
+++ sites/default/themes/lotusPD/page-cart-checkout-complete.tpl.php
@@ -0,0 +1,13 @@
+<?php include("includes/header.inc"); ?>
+			
+				<?php if (!empty($tabs)) { echo $tabs; }; ?>
+				<?php if (!empty($tabs2)) { echo $tabs2; } ?>
+				<?php if (!empty($help)) { echo $help; } ?>
+				<?php if (!empty($messages) && $node->type != "product") { echo $messages; } ?>
+				<?php //passMessages($messages);?>
+			<div class="content-body clearfix">
+			
+				<?php print $content; ?>
+							
+			</div>
+<?php include("includes/footer.inc"); ?>
