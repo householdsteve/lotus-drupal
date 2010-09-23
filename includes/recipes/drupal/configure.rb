554a8cc7f9f1cc27e2070c48a8930c85218006ae
Steve McCrumb <steve@pofd.com>
2010-09-23 10:55:40 +0200

    updated to drupal 6.19

--- includes/recipes/drupal/configure.rb
+++
@@ -1,9 +0,0 @@
-namespace :drupal do
-  namespace :configure do
-    task :stage do
-      source = "#{latest_release}/sites/default/settings.#{stage_name}.php"
-      dest = "#{latest_release}/sites/default/settings.php"
-      run "cp #{source} #{dest}"
-    end
-  end
-end
